30bca63bda4a185de996a9b6596dabb79b944abd
Karunakar098 <164976109+Karunakar098@users.noreply.github.com>
2024-06-20 13:05:25 +0530

    Add files via upload

---
+++ predinedLibraries/src/DateAndTime/Math/MathDemo.java
@@ -0,0 +1,19 @@
+package Math;
+
+public class MathDemo {
+
+	public static void main(String[] args) {
+
+		int maxNum = Math.max(20, 30);
+		System.out.println(maxNum);
+
+		System.out.println(Math.min(40, 50));
+		System.out.println(Math.round(10.6));
+
+		System.out.println(Math.sqrt(16));
+		System.out.println(Math.random());
+
+		System.out.println(Math.PI);
+	}
+
+}
